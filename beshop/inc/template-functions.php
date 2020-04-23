b907a3d78ab15a66d4c4a62631200e2c519b1427
Oleksandr Palamarchuk <a.palamar4uk@gmail.com>
2020-04-23 18:01:36 +0300

    Single product page abjustements and styling

--- beshop/inc/template-functions.php
+++ beshop/inc/template-functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Functions which enhance the theme by hooking into WordPress
  *
@@ -11,27 +12,43 @@
  * @param array $classes Classes for the body element.
  * @return array
  */
-function beshop_body_classes( $classes ) {
-	// Adds a class of hfeed to non-singular pages.
-	if ( ! is_singular() ) {
-		$classes[] = 'hfeed';
-	}
+function beshop_body_classes($classes) {
+    // Adds a class of hfeed to non-singular pages.
+    if (!is_singular()) {
+        $classes[] = 'hfeed';
+    }
 
-	// Adds a class of no-sidebar when there is no sidebar present.
-	if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-		$classes[] = 'no-sidebar';
-	}
+    // Adds a class of no-sidebar when there is no sidebar present.
+    if (!is_active_sidebar('sidebar-1')) {
+        $classes[] = 'no-sidebar';
+    }
 
-	return $classes;
+    return $classes;
 }
-add_filter( 'body_class', 'beshop_body_classes' );
+
+add_filter('body_class', 'beshop_body_classes');
 
 /**
  * Add a pingback url auto-discovery header for single posts, pages, or attachments.
  */
 function beshop_pingback_header() {
-	if ( is_singular() && pings_open() ) {
-		printf( '<link rel="pingback" href="%s">', esc_url( get_bloginfo( 'pingback_url' ) ) );
-	}
+    if (is_singular() && pings_open()) {
+        printf('<link rel="pingback" href="%s">', esc_url(get_bloginfo('pingback_url')));
+    }
 }
-add_action( 'wp_head', 'beshop_pingback_header' );
+
+add_action('wp_head', 'beshop_pingback_header');
+
+function beshop_single_product_description() {
+    $product = wc_get_product();
+    $product_details = $product->get_data();
+    set_query_var('description_heading', 'Description');
+    set_query_var('description_content', $product_details['description']);
+    get_template_part('template-parts/single-product-description');
+}
+
+function beshop_single_product_attributes() {
+    $product = wc_get_product();
+
+    wc_display_product_attributes($product);
+}
---
+++ beshop/inc/template-hooks.php
@@ -0,0 +1,32 @@
+<?php
+
+function custom_single_product() {
+
+    remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20);
+    remove_action('woocommerce_after_single_product_summary', 'woocommerce_upsell_display', 15);
+
+    remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_title', 5);
+    remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_rating', 10);
+    remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 10);
+    remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20);
+    remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40);
+    remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_sharing', 50);
+
+    add_action('woocommerce_single_product_summary', 'woocommerce_template_single_title', 5);
+
+    remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10);
+
+    add_action('woocommerce_single_product_summary', 'beshop_single_product_description', 10);
+    add_action('woocommerce_single_product_summary', 'beshop_single_product_attributes', 15);
+
+    add_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 20);
+}
+
+add_action('woocommerce_single_product_summary', 'custom_single_product', 1);
+
+// Removes comments from product page
+add_action('init', 'remove_comment_support');
+
+function remove_comment_support() {
+    remove_post_type_support('product', 'comments');
+}
