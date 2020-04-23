f7b669c4f4e7c1df6c21d905c605e0d8b12371f6
yurish4e <yurish4e@gmail.com>
2020-04-23 18:32:10 +0300

    img fixed

--- beshop/header.php
+++ beshop/header.php
@@ -30,7 +30,7 @@
                 <div class="logo_wrapper">
                     <a href="#" data-target="mobile-demo" class="sidenav-trigger menu_switcher"></a>
                     <a href="<?php echo site_url() ?>" class="logo">
-                        <img src="<?php echo get_template_directory_uri() ?>/img/logo.jpg" alt="">
+                        <img src="<?php echo get_template_directory_uri() ?>/img/footer/logo.svg" alt="">
 					</a>
 				</div>
 				
