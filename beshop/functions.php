b6834ec2df20eec77abd250eb5ac076ed9f14abf
dimitry.kamarnikov <dimitry.kamarnikov@ironsrc.com>
2020-04-24 16:59:28 +0300

    wpautop inside shortcodes disabled

--- beshop/functions.php
+++ beshop/functions.php
@@ -301,3 +301,6 @@ if ( ! function_exists( 'woocommerce_get_product_thumbnail' ) ) {
         return $output;
     }
 }
+remove_filter( 'the_content', 'wpautop' );
+add_filter( 'the_content', 'wpautop' , 99 );
+add_filter( 'the_content', 'shortcode_unautop', 100 );
