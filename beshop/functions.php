8365abcff9b875152c289f1e722321491897139a
mosquid <dmitrymosquid@MacBook-Pro.local>
2020-06-10 13:41:21 +0300

    svg upload allowed

--- beshop/functions.php
+++ beshop/functions.php
@@ -334,3 +334,19 @@ function bbloomer_remove_billing_postcode_checkout( $fields ) {
   unset($fields['billing']['billing_postcode']);
   return $fields;
 }
+
+function cc_mime_types($mimes) {
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+  }
+  add_filter('upload_mimes', 'cc_mime_types');
+
+  function fix_svg_thumb_display() {
+    echo '
+      td.media-icon img[src$=".svg"], img[src$=".svg"].attachment-post-thumbnail { 
+        width: 100% !important; 
+        height: auto !important; 
+      }
+    ';
+  }
+  add_action('admin_head', 'fix_svg_thumb_display');
