45d0d92d3a2290db1e82b8c9fb5f2bbbf4efef7e
Oleksandr Palamarchuk <a.palamar4uk@gmail.com>
2020-06-24 17:40:22 +0300

    Add products to cart from link

--- beshop/inc/template-hooks.php
+++ beshop/inc/template-hooks.php
@@ -30,3 +30,48 @@ add_action('init', 'remove_comment_support');
 function remove_comment_support() {
     remove_post_type_support('product', 'comments');
 }
+
+// Action to add several products from the request parameter
+// Forked from WC_Form_Handler::add_to_cart_action
+// Example parameter: ?add-to-cart-products=1298,1260
+add_action('wp_loaded', 'add_to_cart_from_link', 30);
+
+function add_to_cart_from_link($url = false) {
+    if (!isset($_REQUEST['add-to-cart-products'])) {
+        return;
+    }
+
+    wc_nocache_headers();
+
+    $product_ids = explode(',', wp_unslash($_REQUEST['add-to-cart-products']));
+
+    foreach ($product_ids as $pid) {
+        $product_id = apply_filters('woocommerce_add_to_cart_product_id', absint($pid));
+        $adding_to_cart = wc_get_product($product_id);
+        if (!$adding_to_cart) {
+            continue;
+        }
+        $was_added = add_to_cart_handler_simple($product_id);
+    }
+
+    wp_safe_redirect(wc_get_cart_url());
+    exit;
+}
+
+/**
+ * Handle adding simple products to the cart.
+ *
+ * @since 2.4.6 Split from add_to_cart_action.
+ * @param int $product_id Product ID to add to the cart.
+ * @return bool success or not
+ */
+function add_to_cart_handler_simple($product_id) {
+    $quantity = 1;
+    $passed_validation = apply_filters('woocommerce_add_to_cart_validation', true, $product_id, $quantity);
+
+    if ($passed_validation && false !== WC()->cart->add_to_cart($product_id, $quantity)) {
+        wc_add_to_cart_message(array($product_id => $quantity), true);
+        return true;
+    }
+    return false;
+}
