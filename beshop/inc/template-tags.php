609c36f1c032c77bfa1deeee52d7d5a909de2ff8
Oleksandr Palamarchuk <a.palamar4uk@gmail.com>
2020-06-30 13:43:58 +0300

    Edit react link added

--- beshop/inc/template-tags.php
+++ beshop/inc/template-tags.php
@@ -6,160 +6,173 @@
  *
  * @package beshop
  */
+if (!function_exists('beshop_posted_on')) :
 
-if ( ! function_exists( 'beshop_posted_on' ) ) :
-	/**
-	 * Prints HTML with meta information for the current post-date/time.
-	 */
-	function beshop_posted_on() {
-		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-		}
+    /**
+     * Prints HTML with meta information for the current post-date/time.
+     */
+    function beshop_posted_on() {
+        $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+        if (get_the_time('U') !== get_the_modified_time('U')) {
+            $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+        }
 
-		$time_string = sprintf(
-			$time_string,
-			esc_attr( get_the_date( DATE_W3C ) ),
-			esc_html( get_the_date() ),
-			esc_attr( get_the_modified_date( DATE_W3C ) ),
-			esc_html( get_the_modified_date() )
-		);
+        $time_string = sprintf(
+                $time_string, esc_attr(get_the_date(DATE_W3C)), esc_html(get_the_date()), esc_attr(get_the_modified_date(DATE_W3C)), esc_html(get_the_modified_date())
+        );
 
-		$posted_on = sprintf(
-			/* translators: %s: post date. */
-			esc_html_x( 'Posted on %s', 'post date', 'beshop' ),
-			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
-		);
+        $posted_on = sprintf(
+                /* translators: %s: post date. */
+                esc_html_x('Posted on %s', 'post date', 'beshop'), '<a href="' . esc_url(get_permalink()) . '" rel="bookmark">' . $time_string . '</a>'
+        );
 
-		echo '<span class="posted-on">' . $posted_on . '</span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+        echo '<span class="posted-on">' . $posted_on . '</span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+    }
 
-	}
 endif;
 
-if ( ! function_exists( 'beshop_posted_by' ) ) :
-	/**
-	 * Prints HTML with meta information for the current author.
-	 */
-	function beshop_posted_by() {
-		$byline = sprintf(
-			/* translators: %s: post author. */
-			esc_html_x( 'by %s', 'post author', 'beshop' ),
-			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-		);
+if (!function_exists('beshop_posted_by')) :
 
-		echo '<span class="byline"> ' . $byline . '</span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+    /**
+     * Prints HTML with meta information for the current author.
+     */
+    function beshop_posted_by() {
+        $byline = sprintf(
+                /* translators: %s: post author. */
+                esc_html_x('by %s', 'post author', 'beshop'), '<span class="author vcard"><a class="url fn n" href="' . esc_url(get_author_posts_url(get_the_author_meta('ID'))) . '">' . esc_html(get_the_author()) . '</a></span>'
+        );
 
-	}
+        echo '<span class="byline"> ' . $byline . '</span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+    }
+
 endif;
 
-if ( ! function_exists( 'beshop_entry_footer' ) ) :
-	/**
-	 * Prints HTML with meta information for the categories, tags and comments.
-	 */
-	function beshop_entry_footer() {
-		// Hide category and tag text for pages.
-		if ( 'post' === get_post_type() ) {
-			/* translators: used between list items, there is a space after the comma */
-			$categories_list = get_the_category_list( esc_html__( ', ', 'beshop' ) );
-			if ( $categories_list ) {
-				/* translators: 1: list of categories. */
-				printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'beshop' ) . '</span>', $categories_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-			}
+if (!function_exists('beshop_entry_footer')) :
 
-			/* translators: used between list items, there is a space after the comma */
-			$tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'beshop' ) );
-			if ( $tags_list ) {
-				/* translators: 1: list of tags. */
-				printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'beshop' ) . '</span>', $tags_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-			}
-		}
+    /**
+     * Prints HTML with meta information for the categories, tags and comments.
+     */
+    function beshop_entry_footer() {
+        // Hide category and tag text for pages.
+        if ('post' === get_post_type()) {
+            /* translators: used between list items, there is a space after the comma */
+            $categories_list = get_the_category_list(esc_html__(', ', 'beshop'));
+            if ($categories_list) {
+                /* translators: 1: list of categories. */
+                printf('<span class="cat-links">' . esc_html__('Posted in %1$s', 'beshop') . '</span>', $categories_list); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+            }
 
-		if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-			echo '<span class="comments-link">';
-			comments_popup_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: post title */
-						__( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'beshop' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					wp_kses_post( get_the_title() )
-				)
-			);
-			echo '</span>';
-		}
+            /* translators: used between list items, there is a space after the comma */
+            $tags_list = get_the_tag_list('', esc_html_x(', ', 'list item separator', 'beshop'));
+            if ($tags_list) {
+                /* translators: 1: list of tags. */
+                printf('<span class="tags-links">' . esc_html__('Tagged %1$s', 'beshop') . '</span>', $tags_list); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+            }
+        }
 
-		edit_post_link(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Edit <span class="screen-reader-text">%s</span>', 'beshop' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			),
-			'<span class="edit-link">',
-			'</span>'
-		);
-	}
+        if (!is_single() && !post_password_required() && ( comments_open() || get_comments_number() )) {
+            echo '<span class="comments-link">';
+            comments_popup_link(
+                    sprintf(
+                            wp_kses(
+                                    /* translators: %s: post title */
+                                    __('Leave a Comment<span class="screen-reader-text"> on %s</span>', 'beshop'), array(
+                'span' => array(
+                    'class' => array(),
+                ),
+                                    )
+                            ), wp_kses_post(get_the_title())
+                    )
+            );
+            echo '</span>';
+        }
+
+        edit_post_link(
+                sprintf(
+                        wp_kses(
+                                /* translators: %s: Name of current post. Only visible to screen readers */
+                                __('Edit WP <span class="screen-reader-text">%s</span>', 'beshop'), array(
+            'span' => array(
+                'class' => array(),
+            ),
+                                )
+                        ), wp_kses_post(get_the_title())
+                ), '<span class="edit-link">', '</span>'
+        );
+
+        // Link to edit product in the react app
+        $user = wp_get_current_user();
+        global $product;
+        if ('product' === get_post_type() && array_intersect(array('administrator'), $user->roles)) {
+            $product->get_id();
+            echo '<div><a href="/manage/#/products/' . $product->get_sku() . '" style="color: red;">React edit</a></div>';
+        }
+    }
+
 endif;
 
-if ( ! function_exists( 'beshop_post_thumbnail' ) ) :
-	/**
-	 * Displays an optional post thumbnail.
-	 *
-	 * Wraps the post thumbnail in an anchor element on index views, or a div
-	 * element when on single views.
-	 */
-	function beshop_post_thumbnail() {
-		if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
-			return;
-		}
+if (!function_exists('beshop_post_thumbnail')) :
 
-		if ( is_singular() ) :
-			?>
+    /**
+     * Displays an optional post thumbnail.
+     *
+     * Wraps the post thumbnail in an anchor element on index views, or a div
+     * element when on single views.
+     */
+    function beshop_post_thumbnail() {
+        if (post_password_required() || is_attachment() || !has_post_thumbnail()) {
+            return;
+        }
 
-			<div class="post-thumbnail">
-				<?php the_post_thumbnail(); ?>
-			</div><!-- .post-thumbnail -->
+        if (is_singular()) :
+            ?>
 
-		<?php else : ?>
+            <div class="post-thumbnail">
+              <?php the_post_thumbnail(); ?>
+            </div><!-- .post-thumbnail -->
 
-			<a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true" tabindex="-1">
-				<?php
-					the_post_thumbnail(
-						'post-thumbnail',
-						array(
-							'alt' => the_title_attribute(
-								array(
-									'echo' => false,
-								)
-							),
-						)
-					);
-				?>
-			</a>
+        <?php else : ?>
 
-			<?php
-		endif; // End is_singular().
-	}
+            <a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true" tabindex="-1">
+                <?php
+                the_post_thumbnail(
+                        'post-thumbnail', array(
+                    'alt' => the_title_attribute(
+                            array(
+                                'echo' => false,
+                            )
+                    ),
+                        )
+                );
+                ?>
+            </a>
+
+        <?php
+        endif; // End is_singular().
+    }
+
 endif;
 
-if ( ! function_exists( 'wp_body_open' ) ) :
-	/**
-	 * Shim for sites older than 5.2.
-	 *
-	 * @link https://core.trac.wordpress.org/ticket/12563
-	 */
-	function wp_body_open() {
-		do_action( 'wp_body_open' );
-	}
+if (!function_exists('wp_body_open')) :
+
+    /**
+     * Shim for sites older than 5.2.
+     *
+     * @link https://core.trac.wordpress.org/ticket/12563
+     */
+    function wp_body_open() {
+        do_action('wp_body_open');
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 endif;
